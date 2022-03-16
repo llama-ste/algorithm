c7d0d6ccc8b5dc2867d50922ef9ad36682f5e73c
llama-ste <andong1212@gmail.com>
2022-03-16 15:03:07 +0900

    조금 더 간단한 버전으로 수정했다.

--- leetcode/day5-hash_table/top_k_frequent_elements.py
+++ leetcode/day5-hash_table/top_k_frequent_elements.py
@@ -9,15 +9,9 @@ class Solution:
         # Counter()를 이용하면 배열의 중첩된 요소를 카운팅하여 딕셔너리로 반환해준다.
         # key는 중첩된 요소, value는 중첩된 개수가 들어있다.
         # most_common()를 이용하면 중첩이 많은 순으로 정렬되고 인자를 넣는다면 가장 많은 순서대로 인자만큼 끊어서 보여준다.
-        most = Counter(nums).most_common(k)
+        mosts = Counter(nums).most_common(k)
 
-        result = []
-
-        # [(k, v), (k, v)]로 담겨있기 때문에 for문을 이용하여 key만 사용했다.
-        for key, value in most:
-            result.append(key)
-
-        return result
+        return [key for key, _ in mosts]
 
 
 sol = Solution()
