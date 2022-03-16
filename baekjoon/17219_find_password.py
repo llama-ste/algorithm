480ca65fd81f6f428bda8c121299ddc7f6e0d8bc
llama-ste <andong1212@gmail.com>
2022-03-16 20:52:20 +0900

    17219 비밀번호 찾기 - https://www.acmicpc.net/problem/17219

---
+++ baekjoon/17219_find_password.py
@@ -0,0 +1,16 @@
+from sys import stdin, stdout
+# 입력 출력을 가속화 한것이다.
+input = stdin.readline
+print = stdout.write
+
+N, M = map(int, input().split())
+sites = {}
+
+# input으로 받는 site, password를 딕셔너리에 key: value로 저장한다.
+for _ in range(N):
+    site, password = input().split()
+    sites[site] = password
+
+# input으로 받는 원하는 site를 키로 넣어 비밀번호를 출력한다.
+for _ in range(M):
+    print(sites[input().rstrip()])
