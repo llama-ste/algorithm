61950de338cdfbe73d05bef7abe8f121bd7c195a
llama-ste <andong1212@gmail.com>
2022-03-16 20:56:02 +0900

    오타 수정

--- baekjoon/17219_find_password.py
+++ baekjoon/17219_find_password.py
@@ -8,7 +8,7 @@ sites = {}
 
 # input으로 받는 site, password를 딕셔너리에 key: value로 저장한다.
 for _ in range(N):
-    site, password = input().split()
+    site, password = input().split(" ")
     sites[site] = password
 
 # input으로 받는 원하는 site를 키로 넣어 비밀번호를 출력한다.
