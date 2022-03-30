bd8f5804c218fa87e5b923716cdcb0e72b42c3af
llama-ste <andong1212@gmail.com>
2022-03-30 11:16:37 +0900

    heapq를 이용한 방법 추가

--- leetcode/day15-heapsort/k-closet-poins-to-origin.py
+++ leetcode/day15-heapsort/k-closet-poins-to-origin.py
@@ -10,7 +10,8 @@ class Solution:
         res = deque([])
         for elem in points:
             # 각요소를 제곱하여 거리값을 구하고 배열로 거리값, 원래좌표를 넣어줬다.
-            res.append([[elem[0] * elem[0] + elem[1] * elem[1]], elem])
+            # 파이썬이기 때문에 루트를 굳이 씌워주지 않았다.
+            res.append([[elem[0] ** 2 + elem[1] ** 2], elem])
 
         sorting = deque(sorted(res))
 
@@ -19,3 +20,21 @@ class Solution:
             answer.append(sorting.popleft()[1])
 
         return answer
+
+
+# heapq를 이용하여 푼 방법이다.
+# import heapq
+#
+# def cal_dist(point):
+#     return math.sqrt(point[0] * point[0] + point[1] * point[1])
+
+# def k_closest_builtin(points, k):
+#     dists = []
+#     heap = []
+#     for point in points:
+#         dist = cal_dist(point)
+#         heapq.heappush(heap, dist)
+#         dists.append(dist)
+#
+#     kth_dist = [heapq.heappop(heap) for _ in range(k)][-1]
+#     return [points[idx] for idx, dist in enumerate(dists) if dist <= kth_dist]
