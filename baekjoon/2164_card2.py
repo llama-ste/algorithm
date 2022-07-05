90bc5ddea989a081c13228c6b8159c895fded6a9
llama-ste <andong1212@gmail.com>
2022-07-05 19:47:29 +0900

    백준 2164번

--- baekjoon/2164_card2.py
+++ baekjoon/2164_card2.py
@@ -1,18 +1,15 @@
 from collections import deque
 
-# preInput = int(input())
-preInput = 100
+nums = int(input())
 
+# range의 두번째 인자는 해당 숫자의 앞까지 자르기 때문에 +1을 해준다.
+# range를 이용하면 배열로 나오기 때문에 반복문을 돌 필요도 없다.
+# deque를 이용하면 popleft(), appendleft()가 사용가능하다 - 시간복잡도 O(1)
+deq = deque(range(1, nums + 1))
 
-def sol(num):
-    # range의 두번째 인자는 해당 숫자의 앞까지 자르기 때문에 +1을 해준다.
-    # deque를 이용하면 popleft(), appendleft()가 사용가능하다 - 시간복잡도 O(1)
-    deq = deque([i for i in range(1, num+1)])
-    while len(deq) > 1:
-        # deq[0]을 빼고, 그다음 deq[0]을 젤 뒤로 보내는 것이다.
-        deq.popleft()
-        deq.append(deq.popleft())
-    return deq.popleft()
+while len(deq) > 1:
+    # deq[0]을 빼고, 그다음 deq[0]을 젤 뒤로 보내는 것이다.
+    deq.popleft()
+    deq.append(deq.popleft())
 
-
-print(sol(preInput))
+print(deq.pop())
